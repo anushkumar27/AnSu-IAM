8ffa642ddcff02ef71596de2f26a4ac69958dc2a
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-10-09 10:45:40 +0530

    Resource classes

---
+++ src/main/java/com/ansu/ansu_iam/TokenResource.java
@@ -0,0 +1,5 @@
+package com.ansu.ansu_iam;
+
+public class TokenResource {
+
+}
---
+++ src/main/java/com/ansu/ansu_iam/UserResource.java
@@ -0,0 +1,60 @@
+package com.ansu.ansu_iam;
+
+import java.sql.SQLException;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/user")
+public class UserResource {
+
+	@POST
+	@Path("/create")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser(
+			@FormParam("name") String name,
+			@FormParam("pass") String password,
+			@FormParam("appId") String appId) {
+		JsonObject resObj = null;
+		
+		try {
+			Model m = new Model();
+			resObj = m.createUser(name, password, appId);
+		} catch (ClassNotFoundException | SQLException e) {
+			resObj = Json.createObjectBuilder()
+					.add("status", "Error")
+					.add("payload", "Internal Server Error, please contact admins")
+					.build();
+		}
+				
+		return Response.ok(resObj.toString(), MediaType.APPLICATION_JSON).build();
+	}
+	
+	@DELETE
+	@Path("/delete")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteUser(
+			@FormParam("uid") String uid,
+			@FormParam("appId") String appId) {
+		JsonObject resObj = null;
+		
+		try {
+			Model m = new Model();
+			resObj = m.removeUser(appId, uid);
+		} catch (ClassNotFoundException | SQLException e) {
+			resObj = Json.createObjectBuilder()
+					.add("status", "Error")
+					.add("payload", "Internal Server Error, please contact admins")
+					.build();
+		}
+				
+		return Response.ok(resObj.toString(), MediaType.APPLICATION_JSON).build();
+	}
+}
